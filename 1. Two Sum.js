8898181637e0507b7f02e9ef8460cd70bb42d772
Diyorbek <diyorbekabdumannonov4@gmail.com>
2023-03-25 15:48:08 +0500

    init

---
+++ 1. Two Sum.js
@@ -0,0 +1,10 @@
+const twoSum = function (nums, target) {
+    const obj = {};
+    for (let i = 0; i < nums.length; i++) {
+        const complement = target - nums[i];
+        if (obj.hasOwnProperty(complement)) {
+            return [obj[complement], i];
+        }
+        obj[nums[i]] = i;
+    }
+};
